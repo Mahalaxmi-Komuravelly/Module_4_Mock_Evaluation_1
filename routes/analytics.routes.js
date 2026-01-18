a5425d60c90d823b0c725774c288538b672015db
Mahalaxmi-Komuravelly <mahalaxmikomuravelly@gmail.com>
2026-01-18 20:21:47 +0530

    analytics cancelled  orders

--- routes/analytics.routes.js
+++ routes/analytics.routes.js
@@ -13,3 +13,17 @@ AnalyticsRoutes.get("/allorders",(req,res)=>{
     })
     res.status(200).json({message:"All orders list and count of orders",count,orders:data.orders})
 })
+AnalyticsRoutes.get("/cancelled-orders",(req,res)=>{
+    const data = readData();
+    if (!data) {
+        return res.status(500).json({ message: "Data Unavailable" })
+    }
+    const cancelledOrders = data.orders.filter((o)=>o.status === "cancelled");
+    if(cancelledOrders.length===0){
+        return res.status(400).json({message:"There are no cancelled orders"})
+    }
+    else{
+    res.status(200).json({message:"All cancelled orders",cancelledOrders})
+
+    }
+})
